68b76cef3b35abd30156188bf4e48f746d5507f1
daiki_yamada <aristrat@icloud.com>
2023-01-13 02:23:16 +0900

    jmapプラグイン導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
+import "rice_map/jmap.js"
 
 Rails.start()
 Turbolinks.start()
