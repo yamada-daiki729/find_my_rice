ae54aa2207ca2a88a022bdebb1c52fe90c9e0adf
daiki_yamada <aristrat@icloud.com>
2023-02-21 02:34:06 +0900

    カスタムバリデート完了

--- app/controllers/rices_controller.rb
+++ app/controllers/rices_controller.rb
@@ -2,6 +2,7 @@ class RicesController < ApplicationController
 
   def rice_map
     @search_rices_form = SearchRicesForm.new(search_params)
+    byebug
     @rices = @search_rices_form.search
     @status_category = [['硬めで甘い',1], ['硬めであっさり',2],['柔らかくて甘い',3],['柔らかくてあっさり',4]]
   end
@@ -13,7 +14,7 @@ class RicesController < ApplicationController
     @rice = Rice.find(params[:id]) #あとでprivateに出す
   end
 
-  def rice_map_serch
+  def rice_map_serch #rice_mapから送られてきたリクエストに対して送り返すメソッド
     @map_serch_rices = Rice.includes(:prefectures).where(rice_prefectures:{prefecture_id: params[:prefectureId].to_i})
     respond_to do |format|
       format.html { redirect_to :root }
@@ -25,6 +26,7 @@ class RicesController < ApplicationController
   private
 
   def search_params
+    byebug
     params[:q]&.permit(:name, :rice_prefecture, :status_category)
   end
 
--- app/forms/search_rices_form.rb
+++ app/forms/search_rices_form.rb
@@ -6,7 +6,10 @@ class SearchRicesForm
   attribute :rice_prefecture, :integer
   attribute :status_category, :integer
 
+  validate :office_is_exist
+
   def search
+    return [] if invalid?
     relation = Rice.distinct
     relation = relation.search_name(name) if name.present?
     relation = relation.search_prefecture(rice_prefecture) if rice_prefecture.present?
@@ -14,4 +17,10 @@ class SearchRicesForm
     return relation
   end
 
+  def office_is_exist
+    byebug
+        if  attributes[:name].nil? ||attributes[:rice_prefecture].nil? || attributes[:status_category].nil?
+            errors.add("は存在しません。")
+        end
+    end
 end
