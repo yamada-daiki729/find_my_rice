a90730600c9d76e611426a7dd0932c92fc1324b1
daiki_yamada <aristrat@icloud.com>
2023-03-10 03:05:10 +0900

    admin_user新規作成編集機能追加　

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,45 @@
 class Admin::UsersController < Admin::BaseController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, success: 'ユーザーを作成しました'
+    else
+      flash.now[:danger] = 'ユーザー作成に失敗しました'
+    end
+  end
+
   def index
-    @users = User.all
+    @users = User.all.order("id")
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    byebug
+    if @user.update(user_params)
+      redirect_to admin_users_path, success: 'ユーザー情報を更新しました'
+    else
+      flash.now[:danger] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
     @user= User.find(params[:id])
     @user.destroy!
     redirect_back_or_to(admin_users_path, danger: 'ログアウトしました')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :password, :password_confirmation)
   end
 end
