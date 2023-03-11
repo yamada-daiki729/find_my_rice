228c411365536c00c51e425059cca68ffa8cf2c9
daiki_yamada <aristrat@icloud.com>
2023-03-12 04:22:31 +0900

    twitter share ogp設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Find_my_rice',
+      title: 'お気に入りのお米を探す検索サービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'find_my_riceを使えば、全国のいろいろなお米を探すことができます。',
+      keywords: 'お米,全国,白米,お米総選挙',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('brandmark-design-1200x630.png'), # 配置するパスやファイル名によって変更すること
+        local: 'ja-JP'
+      },
+      # Twitter用の設定を個別で設定する
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+        image: image_url('brandmark-design-630x630.png') # 配置するパスやファイル名によって変更すること
+      }
+    }
+  end
+
 end
