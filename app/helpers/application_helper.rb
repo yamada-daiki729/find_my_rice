db07fcc02dde8dd61dc537973d4321c529564161
daiki_yamada <aristrat@icloud.com>
2023-04-03 21:11:32 +0900

    ogp画像の変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,14 +17,14 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('brandmark-design-1200x630.png'), # 配置するパスやファイル名によって変更すること
+        image: image_url('brandmark-design-yellow.png'), # 配置するパスやファイル名によって変更すること
         local: 'ja-JP'
       },
       # Twitter用の設定を個別で設定する
       twitter: {
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
         site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('brandmark-design-630x630.png') # 配置するパスやファイル名によって変更すること
+        image: image_url('brandmark-design-yellow.png') # 配置するパスやファイル名によって変更すること
       }
     }
   end
